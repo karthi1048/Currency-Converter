1d25fb2e490305e5a964783e8d0974d0a40a8fd0
karthi1048 <kesavankarthikeyan47@gamil.com>
2024-11-25 18:16:33 +0530

    Uploading files from local repo

---
+++ lists.js
@@ -0,0 +1,9 @@
+const countryList = {
+    AED : 'AE',
+    AUD : 'AU',
+    EUR : 'EU',
+    USD : 'US',
+    INR : 'IN',
+    AFN : 'AF',
+    XCD : 'AG'
+};
---
+++ script.js
@@ -0,0 +1,11 @@
+// const baseURL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json';
+
+const dropdowns = document.querySelector('.dropdown select');
+
+for (code in countryList) {
+    console.log(code);
+}
+
+for( let select of dropdowns) {
+
+}
